85ceab6635a38801a23ecfea58325acad30a4639
Galip Ümit Yolcu <gumityolcu@gmail.com>
2019-03-01 00:30:03 +0300

    Create main.cpp

---
+++ ElementaryCellularAutomata/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<bool>> RunElementaryCellularAutomaton(char W, vector<bool> init);
+
+int main()
+{
+    cout<<sizeof(char);
+    return 0;
+}
+
+vector<vector<bool>> RunElementaryCellularAutomaton(char W, vector<bool> init)
+{
+
+}
